8328c7ed63c99ecef089597b02f0505d5ef41184
ChenchenHu <305369001@qq.com>
2021-04-16 18:31:43 +0800

    first commit

--- Src/SpineSegmentationC3D/loss.py
+++ Src/SpineSegmentationC3D/loss.py
@@ -11,14 +11,13 @@ class Loss(nn.Module):
         self.soft_dice = SoftDiceLoss.SoftDiceLoss(num_classes=20, eps=1e-10)
 
     def forward(self, prediction, gt):
-        with torch.no_grad():
-            pred_A = prediction[0].detach().cpu().numpy()
-            pred_B = prediction[1].detach().cpu().numpy()
-            gt_mask = gt[0]
+        pred_A = prediction[0]
+        pred_B = prediction[1]
+        gt_mask = gt[0]
 
-            pred_A_loss = self.soft_dice(pred_A, gt_mask)
-            pred_B_loss = self.soft_dice(pred_B, gt_mask)
+        pred_A_loss = self.soft_dice(pred_A, gt_mask)
+        pred_B_loss = self.soft_dice(pred_B, gt_mask)
 
-            L1_loss = 0.5 * pred_A_loss + pred_B_loss
+        L1_loss = 0.5 * pred_A_loss + pred_B_loss
 
         return L1_loss
