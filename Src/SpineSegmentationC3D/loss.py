0caded583cabb67d718ea7a25c3b2b6233e5e438
ChenchenHu <305369001@qq.com>
2021-04-17 11:10:29 +0800

    L1 loss

--- Src/SpineSegmentationC3D/loss.py
+++ Src/SpineSegmentationC3D/loss.py
@@ -7,19 +7,19 @@ import torch
 class Loss(nn.Module):
     def __init__(self):
         super().__init__()
-        # self.L1_loss_func = nn.L1Loss(reduction='mean')
+        self.L1_loss_func = nn.L1Loss(reduction='mean')
         # self.ce_loss_func = nn.CrossEntropyLoss()  # need help
-        self.soft_dice = SoftDiceLoss.SoftDiceLoss(num_classes=20, eps=1e-10)
+        # self.soft_dice = SoftDiceLoss.SoftDiceLoss(num_classes=20, eps=1e-10)
 
     def forward(self, prediction, gt):
         pred_A = prediction[0]
         pred_B = prediction[1]
         gt_mask = gt[0]
 
-        pred_A_loss = self.soft_dice(pred_A, gt_mask)
-        pred_B_loss = self.soft_dice(pred_B, gt_mask)
-        # pred_A_loss = self.L1_loss_func(pred_A, gt_mask)
-        # pred_B_loss = self.L1_loss_func(pred_B, gt_mask)
+        # pred_A_loss = self.soft_dice(pred_A, gt_mask)
+        # pred_B_loss = self.soft_dice(pred_B, gt_mask)
+        pred_A_loss = self.L1_loss_func(pred_A, gt_mask)
+        pred_B_loss = self.L1_loss_func(pred_B, gt_mask)
 
         loss = 0.5 * pred_A_loss + pred_B_loss
 
