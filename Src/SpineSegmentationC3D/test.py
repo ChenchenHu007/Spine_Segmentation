8335a6f60a08986937412ea6c9cd8dd44a7a318f
ChenchenHu <305369001@qq.com>
2021-04-21 20:06:39 +0800

    a trial

--- Src/SpineSegmentationC3D/test.py
+++ Src/SpineSegmentationC3D/test.py
@@ -163,7 +163,7 @@ if __name__ == "__main__":
     print('\n\n# Start inference !')
     Spine_Segmentation = '../../Data/Spine_Segmentation'
     cases = sorted(os.listdir(Spine_Segmentation))
-    list_case_dirs = [os.path.join(Spine_Segmentation, cases[i]) for i in range(10, 20)]
+    list_case_dirs = [os.path.join(Spine_Segmentation, cases[i]) for i in range(150, 172)]
     inference(trainer, list_case_dirs, save_path=os.path.join(trainer.setting.output_dir, 'Prediction'),
               do_TTA=args.TTA)
 
